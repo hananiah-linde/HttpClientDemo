8faef15aefc36c2b55a3bac3390f4fc538aec2e5
hananiah-linde <hananiah.linde@gmail.com>
2026-02-23 15:47:05 -0600

    update

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,17 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
-
-app.UseHttpsRedirection();
-
-app.MapGet("/bad", () =>
+builder.Services.AddHttpClient("PingClient", client =>
 {
-    using var client = new HttpClient();
-
+    client.BaseAddress = new Uri("http://localhost:5001/");
 });
 
-app.MapGet("/better", () => "This is a good endpoint");
+var app = builder.Build();
 
-app.MapGet("/best", () => "This is the best endpoint");
+app.MapGet("/bad", async () =>
+{
+    using var client = new HttpClient();
+    Console.WriteLine("sending request to /bad");
+
+    return await client.GetStringAsync("http://localhost:5001/ping");
+});
+
+app.MapGet("/better", (HttpClient client) =>
+{
+    Console.WriteLine("sending request to /better");
+    return client.GetStringAsync("http://localhost:5001/ping");
+});
+
+app.MapGet("/best", (IHttpClientFactory clientFactory) =>
+{
+    var client = clientFactory.CreateClient("PingClient");
+    Console.WriteLine("sending request to /best");
+    return client.GetStringAsync("ping");
+});
 
 app.Run();
